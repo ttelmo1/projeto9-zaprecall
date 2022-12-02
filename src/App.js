83cc025a2c8629427adb7a5309acd447004b5cba
ttelmo1 <telmobsj@gmail.com>
2022-12-02 10:35:48 -0300

    bonus 1 tela inicial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import { useState } from "react";
 import GlobalStyle from "./globalStyles";
 import styled from "styled-components";
-// import questions from "./questions"
 import logo from "./Assets/img/logo.png"
 import Cards from "./Components/Cards"
 import Concluded from "./Components/Concluded"
-import Welcome from "./Components/Welcome"
+
 
 
 export default function App() {
   const [concluded ,setConcluded] = useState(0)
+  const [initialScreen, setInitialScreen] = useState(true)
+  const [cardsScreen, setCardsScreen] = useState(false)
+  const [content, setContent] = useState('center')
 
+  function startRecall(){
+    setInitialScreen(false)
+    setCardsScreen(true)
+    setContent('')
+  }
 
   return (
     <>
       <GlobalStyle />
-      <ScreenContainer>
-        <LogoContainer>
+      <ScreenContainer content={content}>
+       {cardsScreen && 
+       <LogoContainer>
           <img src={logo}/>
           <h1>ZapRecall</h1>
-        </LogoContainer>
-        {/* <Welcome /> */}
-        <Cards setConcluded={setConcluded} concluded={concluded} />
-        <Concluded concluded={concluded} />
+        </LogoContainer>}
+        {initialScreen && 
+          <Welcome>
+            <img src={logo} />
+            <h1>ZapRecall</h1>
+            <button onClick={()=> startRecall()}>Iniciar Recall!</button>
+          </Welcome>
+        }
+        {cardsScreen && <Cards setConcluded={setConcluded} concluded={concluded} />}
+        {cardsScreen && <Concluded concluded={concluded} />}
       </ScreenContainer>
     </>
   );
@@ -37,6 +51,7 @@ const ScreenContainer = styled.div`
   align-items: center;
   margin: 0px;
   padding: 0px;
+  justify-content: ${props => props.content}
   //padding-bottom: 200px; //Estava aumentando o tamnho da tela verticalmente de maneira desnecessária(aparentemente)
 `
 const LogoContainer = styled.div`
@@ -58,4 +73,38 @@ const LogoContainer = styled.div`
   cursor: default
 }
 `
+const Welcome = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  
+  > img{
+    width: 136px;
+    height: 161px;
+  }
 
+  > h1 {
+  font-family: 'Righteous';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  color: #FFFFFF
+  }
+
+  > button {
+    width: 246px;
+    height: 54px;
+    border: none;
+    color: #D70900;
+    background-color: #FFFFFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+    border-radius: 5px;
+    font-family: 'Recursive';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+    text-align: center;
+  }
+`
